61cef8a3f43a6fb025a7303dfbefb392784b521a
kapilpipaliya <kapil.pipaliya@yahoo.com>
2020-10-15 21:55:28 +0530

    Update MainActor.cpp

--- ok/src/actor_system/MainActor.cpp
+++ ok/src/actor_system/MainActor.cpp
@@ -1,8 +1,7 @@
 #include "MainActor.hpp"
 #include <chrono>
 namespace ok {
-main_actor_int::behavior_type
-mainActor(main_actor_int::stateful_pointer<mainActorState> self) {
+main_actor_int::behavior_type mainActor(main_actor_int::stateful_pointer<mainActorState> self) {
   return {
       [=](spawn_and_monitor_atom) {},
       [=](shutdown_atom) {},
